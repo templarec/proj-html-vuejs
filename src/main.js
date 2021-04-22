78cccb9dee4e2a097ecc1a3e6cb07e1570efc6b4
Lorenzo <templarec@gmail.com>
2021-04-22 12:03:27 +0200

    Navbar ok

--- src/main.js
+++ src/main.js
@@ -1,30 +1,49 @@
 //proj-html-vuejs - 21/04/21
 // Author: lorenzobernini
-/*$(document).ready(function () {
-
-});*/
-var app = new Vue({
+const app = new Vue({
 	el: '#root',
 	data: {
+		//array background disponibili per il jumbo
+		backgroundHeader: ["./assets/img/theme_slider1_bg-1.jpg", "./assets/img/theme_slider2_bg-1.jpg", "./assets/img/theme_slider3_bg-1.jpg"],
+		//menu della navbar
 		menu: [
 			{
 				titolo: "Home",
-				items: ["Driving School", "High School", "Kinder Garten", "Header Video"]
+				items: [
+					{text: "Driving School"},
+					{text: "High School"},
+					{text: "Kinder Garten"},
+					{text: "Header Video"}
+				]
 			},
 			{
 				titolo: "Courses",
-				items: ["Our Courses", "Single Course", "Instructor Profile", "Become A Teacher"]
+				items: [
+					{text: "Our Courses"},
+					{text: "Single Course"},
+					{text: "Instructor Profile"},
+					{text: "Become A Teacher"}
+				]
 			},
 			{
 				titolo: "About Us"
 			},
 			{
 				titolo: "News",
-				items: ["EduPrime Blog", "Single Post", "Category Posts"]
+				items: [
+					{text: "EduPrime Blog"},
+					{text: "Single Post"},
+					{text: "Category Posts"}
+				]
 			},
 			{
 				titolo: "Pages",
-				items: ["How It Works", "Single Event", "University Shop", "Single Product"]
+				items: [
+					{text: "How it Works"},
+					{text: "Single Event"},
+					{text: "University Shop"},
+					{text: "Single Product"}
+				]
 			},
 			{
 				titolo: "Contact"
@@ -32,7 +51,16 @@ var app = new Vue({
 			{
 				titolo: "Purchase"
 			}
-		]
+		],
+		//background del jumbo corrente random
+		currentBg: {}
+	},
+	mounted() {
+		//metto in current BG un background random
+		let rand = Math.floor(Math.random() * 3);
+		this.currentBg = {
+			backgroundImage: `url('${this.backgroundHeader[rand]}')`
+		};
 	},
 	computed: {},
 	methods: {}
